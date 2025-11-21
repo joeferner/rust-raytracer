01a77a0f7c3166acfcdf239defae18501b471e86
Joe Ferner <joe@fernsroth.com>
2025-11-21 01:44:31 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,20 @@ fn main() {
 }
 
 fn ray_color(ray: Ray) -> Color {
+    if hit_sphere(Vector3::new(0.0, 0.0, -1.0), 0.5, &ray) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = ray.direction.unit();
     let a = 0.5 * (unit_direction.y + 1.0);
     (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
+}
+
+fn hit_sphere(center: Vector3, radius: f64, ray: &Ray) -> bool {
+    let oc = center - ray.origin;
+    let a = ray.direction.dot(&ray.direction);
+    let b = -2.0 * ray.direction.dot(&oc);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
 }
