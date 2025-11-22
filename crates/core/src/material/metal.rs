b726be11fec24c05ffd565576b1418ec2aef5d38
Joe Ferner <joe@fernsroth.com>
2025-11-22 18:02:03 +0000

    metal fuzz

--- crates/core/src/material/metal.rs
+++ crates/core/src/material/metal.rs
@@ -1,5 +1,5 @@
 use crate::{
-    Color, Ray, RenderContext,
+    Color, Ray, RenderContext, Vector3,
     material::{Material, ScatterResult},
     object::HitRecord,
 };
@@ -7,20 +7,27 @@ use crate::{
 #[derive(Debug)]
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        Self { albedo, fuzz }
     }
 }
 
 impl Material for Metal {
-    fn scatter(&self, _ctx: &RenderContext, r_in: &Ray, hit: &HitRecord) -> Option<ScatterResult> {
+    fn scatter(&self, ctx: &RenderContext, r_in: &Ray, hit: &HitRecord) -> Option<ScatterResult> {
         let reflected = r_in.direction.reflect(hit.normal);
-        Some(ScatterResult {
-            attenuation: self.albedo,
-            scattered: Ray::new(hit.pt, reflected),
-        })
+        let reflected = reflected.unit() + (self.fuzz * Vector3::random_unit(ctx));
+        let scattered = Ray::new(hit.pt, reflected);
+        if scattered.direction.dot(&hit.normal) > 0.0 {
+            Some(ScatterResult {
+                attenuation: self.albedo,
+                scattered,
+            })
+        } else {
+            None
+        }
     }
 }
