cf195c2851d40cc67beac15d8b41bee9739f33d9
Joe Ferner <joe@fernsroth.com>
2026-01-04 04:12:27 +0000

    add github logo

--- webapp/frontend/src/components/Header.tsx
+++ webapp/frontend/src/components/Header.tsx
@@ -5,6 +5,7 @@ import { UnstyledButton } from '@mantine/core';
 import type { User } from '../api';
 import { LoginDialog } from './LoginDialog';
 import { useSignal } from '@preact/signals-react';
+import { Github as GithubIcon } from 'react-bootstrap-icons';
 
 const PICTURE_SIZE = 35;
 
@@ -23,6 +24,9 @@ export function Header(): JSX.Element {
         <div className={classes.header}>
             <div className={classes.title}>
                 <img src="/navbar-logo.png" height={40} width={110} />
+                <a href="https://github.com/joeferner/caustic/" target="_blank">
+                    <GithubIcon width={25} height={25} />
+                </a>
             </div>
             <div className={classes.projectName}>
                 {projectStore.project.value && <div>{projectStore.project.value.name}</div>}
